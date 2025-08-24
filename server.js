48ff759ac5796c0b4d4f5a4f1a8f80a905563050
jitu <jitukumar63766@gmail.com>
2025-08-24 11:00:54 +0530

    cors configuration

--- server.js
+++ server.js
@@ -12,12 +12,23 @@ import connectDB from "./config/db.js";
 dotenv.config();
 
 const app = express();
-app.use(
-  cors({
-    origin: "http://localhost:5173", // your React app URL
-    credentials: true, // allow cookies/authorization headers
-  })
-);
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://iat-frontend.vercel.app"
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+}));
+
 
 app.use(express.json());
 
