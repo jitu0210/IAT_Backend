bdf0b30c3f6ebfbe367d3acbe30c2c7e095404e8
jitu <jitukumar63766@gmail.com>
2025-08-30 20:51:12 +0530

    group model fixed

--- models/group.model.js
+++ models/group.model.js
@@ -1,159 +1,233 @@
+// import mongoose from "mongoose";
+
+// const ratingSchema = new mongoose.Schema({
+//   userId: {
+//     type: mongoose.Schema.Types.ObjectId,
+//     ref: 'User',
+//     required: true
+//   },
+//   userName: {
+//     type: String,
+//     required: true
+//   },
+//   communication: {
+//     type: Number,
+//     required: true,
+//     min: 0,
+//     max: 40
+//   },
+//   presentation: {
+//     type: Number,
+//     required: true,
+//     min: 0,
+//     max: 40
+//   },
+//   content: {
+//     type: Number,
+//     required: true,
+//     min: 0,
+//     max: 40
+//   },
+//   helpfulForCompany: {
+//     type: Number,
+//     required: true,
+//     min: 0,
+//     max: 40
+//   },
+//   helpfulForInterns: {
+//     type: Number,
+//     required: true,
+//     min: 0,
+//     max: 40
+//   },
+//   participation: {
+//     type: Number,
+//     required: true,
+//     min: 0,
+//     max: 40
+//   },
+//   comments: {
+//     type: String,
+//     maxlength: 500
+//   },
+//   createdAt: {
+//     type: Date,
+//     default: Date.now,
+//     expires: 604800 // 7 days in seconds (7 * 24 * 60 * 60)
+//   }
+// });
+
+// const groupSchema = new mongoose.Schema({
+//   name: {
+//     type: String,
+//     required: true,
+//     unique: true,
+//     trim: true
+//   },
+//   members: [{
+//     userId: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: 'User'
+//     },
+//     name: {
+//       type: String,
+//       required: true
+//     },
+//     email: {
+//       type: String,
+//       required: true
+//     },
+//     branch: {
+//       type: String,
+//       default: "Not specified"
+//     },
+//     joinDate: {
+//       type: Date,
+//       default: Date.now
+//     }
+//   }],
+//   ratings: [ratingSchema],
+//   totalRating: {
+//     type: Number,
+//     default: 0
+//   },
+//   ratingCount: {
+//     type: Number,
+//     default: 0
+//   },
+//   avgCommunication: {
+//     type: Number,
+//     default: 0
+//   },
+//   avgPresentation: {
+//     type: Number,
+//     default: 0
+//   },
+//   avgContent: {
+//     type: Number,
+//     default: 0
+//   },
+//   avgHelpfulForCompany: {
+//     type: Number,
+//     default: 0
+//   },
+//   avgHelpfulForInterns: {
+//     type: Number,
+//     default: 0
+//   },
+//   avgParticipation: {
+//     type: Number,
+//     default: 0
+//   }
+// }, {
+//   timestamps: true
+// });
+
+// // Update group statistics when a rating is added
+// groupSchema.methods.updateStats = function() {
+//   if (this.ratings.length === 0) {
+//     this.totalRating = 0;
+//     this.ratingCount = 0;
+//     this.avgCommunication = 0;
+//     this.avgPresentation = 0;
+//     this.avgContent = 0;
+//     this.avgHelpfulForCompany = 0;
+//     this.avgHelpfulForInterns = 0;
+//     this.avgParticipation = 0;
+//     return;
+//   }
+
+//   const totalComm = this.ratings.reduce((sum, r) => sum + r.communication, 0);
+//   const totalPres = this.ratings.reduce((sum, r) => sum + r.presentation, 0);
+//   const totalCont = this.ratings.reduce((sum, r) => sum + r.content, 0);
+//   const totalComp = this.ratings.reduce((sum, r) => sum + r.helpfulForCompany, 0);
+//   const totalInt = this.ratings.reduce((sum, r) => sum + r.helpfulForInterns, 0);
+//   const totalPart = this.ratings.reduce((sum, r) => sum + r.participation, 0);
+  
+//   this.ratingCount = this.ratings.length;
+//   this.avgCommunication = totalComm / this.ratingCount;
+//   this.avgPresentation = totalPres / this.ratingCount;
+//   this.avgContent = totalCont / this.ratingCount;
+//   this.avgHelpfulForCompany = totalComp / this.ratingCount;
+//   this.avgHelpfulForInterns = totalInt / this.ratingCount;
+//   this.avgParticipation = totalPart / this.ratingCount;
+//   this.totalRating = (totalComm + totalPres + totalCont + totalComp + totalInt + totalPart) / this.ratingCount;
+// };
+
+// const Group = mongoose.model('Group', groupSchema);
+
+// export default Group;
+
 import mongoose from "mongoose";
+
+const memberSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  name: String,
+  email: String,
+  branch: String,
+});
 
 const ratingSchema = new mongoose.Schema({
   userId: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
-  },
-  userName: {
-    type: String,
-    required: true
-  },
-  communication: {
-    type: Number,
+    ref: "User",
     required: true,
-    min: 0,
-    max: 40
   },
-  presentation: {
-    type: Number,
-    required: true,
-    min: 0,
-    max: 40
-  },
-  content: {
-    type: Number,
-    required: true,
-    min: 0,
-    max: 40
-  },
-  helpfulForCompany: {
-    type: Number,
-    required: true,
-    min: 0,
-    max: 40
-  },
-  helpfulForInterns: {
-    type: Number,
-    required: true,
-    min: 0,
-    max: 40
-  },
-  participation: {
-    type: Number,
-    required: true,
-    min: 0,
-    max: 40
-  },
-  comments: {
-    type: String,
-    maxlength: 500
-  },
+  userName: String,
+  communication: { type: Number, min: 1, max: 5 },
+  presentation: { type: Number, min: 1, max: 5 },
+  content: { type: Number, min: 1, max: 5 },
+  helpfulForCompany: { type: Number, min: 1, max: 5 },
+  helpfulForInterns: { type: Number, min: 1, max: 5 },
+  participation: { type: Number, min: 1, max: 5 },
+  comments: String,
   createdAt: {
     type: Date,
     default: Date.now,
-    expires: 604800 // 7 days in seconds (7 * 24 * 60 * 60)
-  }
+  },
 });
 
-const groupSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true
-  },
-  members: [{
-    userId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User'
-    },
+const groupSchema = new mongoose.Schema(
+  {
     name: {
       type: String,
-      required: true
+      required: true,
+      unique: true,
     },
-    email: {
-      type: String,
-      required: true
+    members: [memberSchema], // ✅ members array
+    ratings: [ratingSchema], // ✅ ratings array
+    totalRating: {
+      type: Number,
+      default: 0,
     },
-    branch: {
-      type: String,
-      default: "Not specified"
-    },
-    joinDate: {
-      type: Date,
-      default: Date.now
-    }
-  }],
-  ratings: [ratingSchema],
-  totalRating: {
-    type: Number,
-    default: 0
   },
-  ratingCount: {
-    type: Number,
-    default: 0
-  },
-  avgCommunication: {
-    type: Number,
-    default: 0
-  },
-  avgPresentation: {
-    type: Number,
-    default: 0
-  },
-  avgContent: {
-    type: Number,
-    default: 0
-  },
-  avgHelpfulForCompany: {
-    type: Number,
-    default: 0
-  },
-  avgHelpfulForInterns: {
-    type: Number,
-    default: 0
-  },
-  avgParticipation: {
-    type: Number,
-    default: 0
-  }
-}, {
-  timestamps: true
-});
+  { timestamps: true }
+);
 
-// Update group statistics when a rating is added
-groupSchema.methods.updateStats = function() {
+// 🔹 Helper method to recalc avg rating
+groupSchema.methods.updateStats = function () {
   if (this.ratings.length === 0) {
     this.totalRating = 0;
-    this.ratingCount = 0;
-    this.avgCommunication = 0;
-    this.avgPresentation = 0;
-    this.avgContent = 0;
-    this.avgHelpfulForCompany = 0;
-    this.avgHelpfulForInterns = 0;
-    this.avgParticipation = 0;
     return;
   }
 
-  const totalComm = this.ratings.reduce((sum, r) => sum + r.communication, 0);
-  const totalPres = this.ratings.reduce((sum, r) => sum + r.presentation, 0);
-  const totalCont = this.ratings.reduce((sum, r) => sum + r.content, 0);
-  const totalComp = this.ratings.reduce((sum, r) => sum + r.helpfulForCompany, 0);
-  const totalInt = this.ratings.reduce((sum, r) => sum + r.helpfulForInterns, 0);
-  const totalPart = this.ratings.reduce((sum, r) => sum + r.participation, 0);
-  
-  this.ratingCount = this.ratings.length;
-  this.avgCommunication = totalComm / this.ratingCount;
-  this.avgPresentation = totalPres / this.ratingCount;
-  this.avgContent = totalCont / this.ratingCount;
-  this.avgHelpfulForCompany = totalComp / this.ratingCount;
-  this.avgHelpfulForInterns = totalInt / this.ratingCount;
-  this.avgParticipation = totalPart / this.ratingCount;
-  this.totalRating = (totalComm + totalPres + totalCont + totalComp + totalInt + totalPart) / this.ratingCount;
+  let totalPoints = 0;
+  this.ratings.forEach((r) => {
+    totalPoints +=
+      r.communication +
+      r.presentation +
+      r.content +
+      r.helpfulForCompany +
+      r.helpfulForInterns +
+      r.participation;
+  });
+
+  this.totalRating = totalPoints / this.ratings.length;
 };
 
-const Group = mongoose.model('Group', groupSchema);
-
+const Group = mongoose.model("Group", groupSchema);
 export default Group;
